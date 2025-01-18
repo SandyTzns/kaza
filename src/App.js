c972278711702363c323e1aab35615735264f195
Sandy-Line <121861083+SandyTzns@users.noreply.github.com>
2025-01-18 22:31:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { Footer } from './components/Footer'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/kaza">
       <Layout>
         <Nav />
         <Routes>
