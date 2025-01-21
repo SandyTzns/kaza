1046d23e9a17eef509d4c7bbfdc7ba62dd5eeaa2
SandyTzns <121861083+SandyTzns@users.noreply.github.com>
2025-01-21 11:35:03 +0100

    Fix: updated import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, HashRouter } from 'react-router-dom'
+import { Routes, Route, HashRouter, Navigate } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { Home } from './pages/Home'
 import { About } from './pages/About'
